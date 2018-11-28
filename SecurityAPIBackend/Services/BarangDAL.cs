9fb2a89ce79ca4b1cfecc84f7c0c694576f02c40
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-28 14:54:15 +0700

    menambahkan select barang

--- SecurityAPIBackend/Services/BarangDAL.cs
+++ SecurityAPIBackend/Services/BarangDAL.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SecurityAPIBackend.Data;
 using SecurityAPIBackend.Models;
 
 namespace SecurityAPIBackend.Services
 {
     public class BarangDAL : IBarang
     {
-        public Task Create(Barang obj)
+        private ApplicationDbContext _db;
+        public BarangDAL(ApplicationDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
+        }
+
+        public async Task Create(Barang obj)
+        {
+            try
+            {
+                _db.Barang.Add(obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public Task Delete(Barang obj)
@@ -18,14 +34,22 @@ namespace SecurityAPIBackend.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Barang>> GetAll()
+        public async Task<IEnumerable<Barang>> GetAll()
         {
-            throw new NotImplementedException();
+            var models = await (from b in _db.Barang
+                          orderby b.NamaBarang ascending
+                          select b).AsNoTracking().ToListAsync();
+            return models;
         }
 
-        public Task<Barang> GetById(string id)
+        public async Task<Barang> GetById(string id)
         {
-            throw new NotImplementedException();
+            var model = await (from b in _db.Barang
+                               where b.KodeBarang == id
+                               select b).AsNoTracking().SingleOrDefaultAsync();
+            if (model == null)
+                throw new Exception($"Data {id} tidak ditemukan");
+            return model;
         }
 
         public Task Update(Barang obj)
