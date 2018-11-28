0c48480dcf1b9f6fcba01e0354cba22da7a24c3f
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-28 15:14:26 +0700

    menambahkan controller

---
+++ SecurityAPIBackend/Controllers/BarangController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecurityAPIBackend.Services;
+
+namespace SecurityAPIBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarangController : ControllerBase
+    {
+        private IBarang _barang;
+        public BarangController(IBarang barang)
+        {
+            _barang = barang;
+        }
+
+        // GET: api/Barang
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Barang/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Barang
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Barang/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
