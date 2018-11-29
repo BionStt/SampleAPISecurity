c74d56c9ad2f0f1924bf2adc189a4cb7c743d781
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-29 09:35:11 +0700

    menambahkan method authenticate

---
+++ SecurityAPIBackend/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecurityAPIBackend.Models;
+using SecurityAPIBackend.Services;
+
+namespace SecurityAPIBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUser _usr;
+        public UserController(IUser usr)
+        {
+            _usr = usr;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]User usr)
+        {
+            await _usr.Register(usr);
+            return Ok();
+        }
+
+        [HttpPost("createrole")]
+        public async Task<IActionResult> CreateRole([FromBody]string roleName)
+        {
+            await _usr.CreateRole(roleName);
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("addusertorole")]
+        public async Task<IActionResult> AddUserToRole([FromBody] UserRole usrRole)
+        {
+            await _usr.AddUserToRole(usrRole.username, usrRole.rolename);
+            return Ok();
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]User userParam)
+        {
+            var user = await _usr.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- SecurityAPIBackend/Services/UserDAL.cs
+++ SecurityAPIBackend/Services/UserDAL.cs
@@ -1,31 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SecurityAPIBackend.Helpers;
 using SecurityAPIBackend.Models;
 
 namespace SecurityAPIBackend.Services
 {
     public class UserDAL : IUser
     {
-        public Task AddUserToRole(string username, string role)
+        private AppSettings _appSettings;
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public UserDAL(IOptions<AppSettings> appSettings, UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
-            throw new NotImplementedException();
+            _appSettings = appSettings.Value;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        public Task<User> Authenticate(string username, string password)
+        public async Task AddUserToRole(string username, string role)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(username);
+            try
+            {
+                await _userManager.AddToRoleAsync(user, role);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error {ex.Message}");
+            }
         }
 
-        public Task<bool> CheckUserInRole(string username, string rolename)
+        public async Task<User> Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            var userFind = await _userManager.CheckPasswordAsync(await _userManager.FindByNameAsync(username), password);
+
+            // return null if user not found
+            if (!userFind)
+                return null;
+
+            var user = new User
+            {
+                Username = username
+            };
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
         }
 
-        public Task CreateRole(string roleName)
+        public async Task<bool> CheckUserInRole(string username, string rolename)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(username);
+            var check = await _userManager.IsInRoleAsync(user, rolename);
+            return check;
+        }
+
+        public async Task CreateRole(string roleName)
+        {
+            IdentityResult roleResult;
+            var roleExist = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExist)
+                roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
         }
 
         public IEnumerable<User> GetAll()
@@ -33,9 +95,12 @@ namespace SecurityAPIBackend.Services
             throw new NotImplementedException();
         }
 
-        public Task Register(User usr)
+        public async Task Register(User usr)
         {
-            throw new NotImplementedException();
+            var user = new IdentityUser { UserName = usr.Username, Email = usr.Username };
+            var result = await _userManager.CreateAsync(user, usr.Password);
+            if (!result.Succeeded)
+                throw new Exception("Gagal menambah Pengguna");
         }
     }
 }
