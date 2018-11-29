3f8125a1e3edd3c9c0c103f584dda4173208f921
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-29 09:39:52 +0700

    menghapus method yang tidak perlu

--- SecurityAPIBackend/Controllers/UserController.cs
+++ SecurityAPIBackend/Controllers/UserController.cs
@@ -60,29 +60,6 @@ namespace SecurityAPIBackend.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/User/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/User
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/User/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
